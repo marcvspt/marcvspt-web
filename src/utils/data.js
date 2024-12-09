aafb66d3f5dfee3999e45b35ca2f1019f7b42aa2
marcvspt <78035694+marcvspt@users.noreply.github.com>
2024-12-08 23:43:35 -0600

    First version

---
+++ src/utils/data.js
@@ -0,0 +1,86 @@
+export const SKILLS_ITEMS = [
+    {
+        label: "Linux",
+    },
+    {
+        label: "Windows",
+    },
+    {
+        label: "Firewall",
+    },
+    {
+        label: "NGFW",
+    },
+    {
+        label: "WAF",
+    },
+    {
+        label: "SIEM",
+    },
+    {
+        label: "XDR",
+    },
+    {
+        label: "EDR",
+    },
+    {
+        label: "SEG",
+    },
+    {
+        label: "Proxy",
+    },
+    {
+        label: "Router",
+    },
+    {
+        label: "Switch",
+    },
+    {
+        label: "IPSec",
+    },
+    {
+        label: "Active Directory",
+    },
+    {
+        label: "Python",
+    },
+    {
+        label: "PowerShell",
+    },
+    {
+        label: "Burpsuite",
+    },
+    {
+        label: "Nmap",
+    },
+    {
+        label: "Netcat",
+    },
+];
+
+export const EXPERIENCE_ITEMS = [
+    {
+        organization: "Organización gubernamental",
+        focus: "",
+        link: "",
+        date: "Noviembre, 2023 - Actualmente",
+        description:
+            "Administración de Firewalls de nueva generación. Configuración de VPN site-to-site y client-to-site. Monitoreo de sistemas EDR, XDR, SIEM, SEG y WAF. Investigación de eventos e incidentes de seguridad. Investigación de vulnerabilidades y su explotabilidad.",
+    },
+    {
+        organization: "31 Rooms",
+        focus: "Prácticas",
+        link: "https://www.conexionesti.com/",
+        date: "Enero, 2023 - Abril, 2023",
+        description:
+            "Proyecto de grado de ingeniería: Pentest a servicios web en modalidad de caja gris. Pruebas de seguridad a dos APIs y dos interfaces web. Determinación de severidad de vulnerbilidad y explotabilidad.",
+    },
+    {
+        organization: "ConexionesTI",
+        focus: "Prácticas",
+        link: "https://31rooms.com/",
+        date: "Mayo, 2021 - Agosto 2021",
+        description:
+            "Proyecto de grado de carrera técnica: Administración e instalación de enlaces inalámbricos mediante radiofrecuencias de un Proveedor de Servicios de Interne Inalámbrico. Administración de routers, access point, switches y antenas 5Ghz. Configuración enlaces inalámbricos punto-a-punto y punto-a-multipunto. Administración de redes LAN, WLAN y WAN.",
+    },
+];
