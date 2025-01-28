bba6e1d9d846fcf9bd7057a37f9392a0a81374d9
marcvspt <78035694+marcvspt@users.noreply.github.com>
2025-01-27 20:29:17 -0600

    (Feature) New tailwind colors theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,32 @@
 export default {
 	darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {}, plugins: [
-	]
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#ffffff', // Green
+					dark: '#374151'
+				},
+				secondary: {
+					DEFAULT: '#DDD6FE', // Gray
+					dark: '#A855F7'
+				},
+				terciary: {
+					DEFAULT: '#F3E8FF',
+					dark: '#C4B5FD'
+				},
+				accent: {
+					DEFAULT: '#8B5CF6',
+					dark: '#5B21B6'
+				},
+				fully: {
+					DEFAULT: '#f3f4f6',
+					dark: '#4c1d95'
+				}
+
+			}
+		},
+	},
+	plugins: []
 }
