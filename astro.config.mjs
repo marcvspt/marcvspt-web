431db1e7142f329bc5669e5c8680a2a63e31ae83
marcvspt <78035694+marcvspt@users.noreply.github.com>
2026-03-26 21:31:22 -0600

    Migrate to astro v6

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.marcvspt.tech/',
-
-  integrations: [],
+  output: 'static',
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
